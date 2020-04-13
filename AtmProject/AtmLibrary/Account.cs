7d2637ad0bc1193e804fb5e363860c8f684d6653
mikeywu5 <wum@uhcl.edu>
2020-04-13 16:46:14 -0500

    ATM Project

---
+++ AtmProject/AtmLibrary/Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtmLibrary
+{
+    public class Account
+    {
+        public int AccountNumber { get; }
+
+        private int _pin;
+
+        private decimal _balance;
+
+        public decimal Balance { get { return _balance; } }
+
+        public Account(int accountNumber, int pin, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            _pin = pin;
+            _balance = balance;
+        }
+
+        public bool ValidatePin(int pin)
+        {
+            return pin == _pin;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            _balance -= amount;
+            return true;
+        }
+
+
+        /// <summary>
+        /// Fetch an account using an account number and a pin
+        /// </summary>
+        /// <param name="accountNumber"></param>
+        /// <param name="pin"></param>
+        /// <returns>account or null if not found / bad pin</returns>
+        public static Account Fetch(int accountNumber, int pin)
+        {
+            foreach (Account account in _database)
+            {
+                if (accountNumber == account.AccountNumber)
+                {
+                    if (account.ValidatePin(pin))
+                    {
+                        return account;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static readonly List<Account> _database = new List<Account>() {
+            new Account(1234567, 7890, 10000m),
+            new Account(7654321, 987, 1000m)
+          };
+    }
+}
---
+++ AtmProject/AtmLibrary/BalanceInquiry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtmLibrary
+{
+    public class BalanceInquiry : Transaction
+    {
+        // delegations - compositions initiated at construction
+        private readonly Account _account;
+        private readonly IScreen _screen;
+
+        public BalanceInquiry(IScreen screen, Account account)
+        {
+            _screen = screen;
+            _account = account;
+        }
+
+        public override string MenuName => "View my balance";
+
+        public override void Execute()
+        {
+            _screen.WriteLine($"You have {_account.Balance:C} left in your account");
+        }
+    }
+}
